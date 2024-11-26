1cf749d5141bf2e95201ba19be2ffa06b28aa78f
Fardaan Mahdi <93463648+Fardaan-Mahdi@users.noreply.github.com>
2024-11-26 20:40:04 +0530

    Update auth.js

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:7001/api/auth";
+const API_URL = "https://vrvsecurities-backend.onrender.com";
 
 export const login = (credentials) => axios.post(`${API_URL}/login`, credentials);
 export const register = (userData) => axios.post(`${API_URL}/register`, userData);
